cae4587738737ae828223e4a06fca92eaaab0ac8
Gabriel GONZALEZ <gagonzal@e2r10p10.42.fr>
2018-01-24 15:29:27 +0100

    ok

--- get_next_line.c
+++ get_next_line.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: gagonzal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/01/18 22:09:38 by gagonzal          #+#    #+#             */
-/*   Updated: 2018/01/23 20:15:13 by gagonzal         ###   ########.fr       */
+/*   Created: 2018/01/24 14:13:08 by gagonzal          #+#    #+#             */
+/*   Updated: 2018/01/24 15:27:02 by gagonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,32 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-int		get_line(char **line, char *src, char c)
+int				get_line(char **line, t_list **src, char c)
 {
-	int size;
-	int i;
-	char *str;
+	int		siz;
+	int		i;
+	char	*str;
+	char	*tmp2;
+	t_list	*tmp;
 
-	size = 0;
-	while (src && src[size] && src[size] != c)
-		size++;
-	str = ft_strnew(size);
-	i = 0;
-	while (str && src[i] && src[i] !=c)
-	{
-		str[i] = src[i];
-		i++;
-	}
+	siz = 0;
+	tmp = *src;
+	while (tmp && ((char*)tmp->content)[siz] && ((char*)tmp->content)[siz] != c)
+		siz++;
+	str = ft_strnew(siz);
+	i = -1;
+	while (str && ((char*)tmp->content)[++i] && ((char*)tmp->content)[i] != c)
+		str[i] = ((char*)tmp->content)[i];
 	*line = str;
-	return (size);
+	if (siz < (int)ft_strlen(tmp->content))
+	{
+		tmp2 = ft_strdup(tmp->content + (siz + 1));
+		free(tmp->content);
+		tmp->content = tmp2;
+	}
+	else
+		ft_strclr(tmp->content);
+	return (1);
 }
 
 static t_list	*get_data(t_list **old_line, int fd)
@@ -44,55 +52,41 @@ static t_list	*get_data(t_list **old_line, int fd)
 	tmp = *old_line;
 	while (tmp)
 	{
-		if ((int)tmp->content_size == fd)
+		if ((int)tmp->fd == fd)
 		{
-			return(tmp);
+			return (tmp);
 		}
 		tmp = tmp->next;
 	}
-	tmp = ft_lstnew("", fd);
+	tmp = ft_lstnew("", sizeof(char*));
+	tmp->fd = fd;
 	ft_lstadd(old_line, tmp);
 	return (tmp);
 }
 
-int		get_next_line(const int fd, char **line)
+int				get_next_line(const int fd, char **line)
 {
-	char buf[BUFF_SIZE + 1];
-	static t_list	*old_line = NULL;
-	int ret;
-	int pos;
-	t_list		*curr;
-	char		*tmp;
+	char			buf[BUFF_SIZE + 1];
+	static	t_list	*old_line = NULL;
+	int				ret;
+	t_list			*curr;
+	char			*tmp;
 
-	if (fd < 0 || !line)
+	if (fd < 0 || line == NULL)
 		return (-1);
 	curr = get_data(&old_line, fd);
 	while ((ret = read(fd, buf, BUFF_SIZE)))
 	{
 		if (ret < 0)
-			return -1;
+			return (-1);
 		buf[ret] = '\0';
 		tmp = ft_strjoin(curr->content, buf);
 		free(curr->content);
 		curr->content = tmp;
 		if (ft_strchr(buf, '\n'))
-			break;
+			break ;
 	}
 	if (ret < BUFF_SIZE && !ft_strlen(curr->content))
-	{
-		free(curr->content);
 		return (0);
-	}
-	pos = get_line(line, curr->content, '\n');
-	if (pos < (int)ft_strlen(curr->content))
-	{
-		tmp = ft_strdup(curr->content + (pos + 1));
-		free(curr->content);
-		curr->content = tmp;
-	}
-	else
-	{
-		ft_strclr(curr->content);
-	}
-	return (1);
+	return (get_line(line, &curr, '\n'));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,15 @@
 /*   By: gagonzal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/08 19:53:07 by gagonzal          #+#    #+#             */
-/*   Updated: 2018/01/22 16:15:05 by gagonzal         ###   ########.fr       */
+/*   Updated: 2018/01/24 15:07:48 by gagonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE	100
+# define BUFF_SIZE	4000
 # include <stdio.h>
+
 int		get_next_line(int fd, char **line);
 
 #endif
